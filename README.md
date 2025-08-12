589b7d74076f30e42acee1c1d9a3cbfb492f48ef
Nils <nils.yseboodt@gmail.com>
2025-08-12 14:10:04 +0200

    Update to support dnd 5.0.4

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Like to roll for HP but don't want to low roll? Then this is the module for you!
 If your character happens to roll below average, it will simply roll again until you either meet the average or supersede it!
 
 ![roll-example](/assets/roll-example.jpg)
+
+## Compatibility
+Tested if this module is working for the following confifurations
+- FVTT 11.315 & DND5e 3.2.1
+- FVTT 12.331 & DND5e 4.4.4
+- FVTT 13.346 & DND5e 5.0.4
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,6 +2,9 @@ interface Item5e extends Item {
   system: {
     [key: string]: any;
     hitDice?: `d${number}`;
+    hd?: {
+      denomination?: `d${number}`;
+    }
   };
 }
 interface RollData {
@@ -12,10 +15,11 @@ interface RollData {
 }
 
 Hooks.on('dnd5e.preRollClassHitPoints', (actor: Actor, classItem: Item5e, rollData: RollData, messageData: Record<string, any>) => {
-  if (!classItem.system.hitDice) {
+  const hdDenomination = classItem.system.hitDice /* old dnd */ ?? classItem.system.hd?.denomination /* new dnd (tested with dnd 5.0.4) */
+  if (!hdDenomination) {
     return;
   }
-  const hd = Number(classItem.system.hitDice.substring(1));
+  const hd = Number(hdDenomination.substring(1));
   if (Number.isNaN(hd)) {
     return;
   }
@@ -27,6 +31,20 @@ Hooks.on('renderHitPointsFlow', (app: FormApplication, html: HTMLElement | JQuer
   if (!(html instanceof HTMLElement)) {
     html = html[0];
   }
-  const htmlButton = html.querySelector('button.rollButton');
-  htmlButton.innerHTML = htmlButton.innerHTML.replace(data.hitDie, `${data.hitDie}rr<${Math.ceil(data.dieValue / 2)+1}`)
+
+  // V1 application: button.rollButton
+  let htmlButton = html.querySelector('button.rollButton');
+  if (htmlButton) {
+    htmlButton.innerHTML = htmlButton.innerHTML.replace(data.hitDie, `${data.hitDie}rr<${Math.ceil(data.dieValue / 2)+1}`)
+  }
+
+  // V2 application: button.roll-button
+  htmlButton = html.querySelector('button.roll-button');
+  if (htmlButton) {
+    for (const attr of [`aria-label`, `data-tooltip`]) {
+      if (htmlButton.hasAttribute(attr)) {
+        htmlButton.setAttribute(attr, htmlButton.getAttribute(attr).replace(data.hitDie, `${data.hitDie}rr<${Math.ceil(data.dieValue / 2)+1}`))
+      }
+    }
+  }
 })
